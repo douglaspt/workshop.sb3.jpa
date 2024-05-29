be83cb71cced2d77f889ededcb6446729a610e84
Douglas Pimentel Teixeira <douglaspt@gmail.com>
2024-05-29 17:13:34 -0300

    Category

---
+++ src/main/java/com/dpt/cursospring/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.dpt.cursospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dpt.cursospring.entities.Category;
+import com.dpt.cursospring.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
