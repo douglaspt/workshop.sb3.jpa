0924c3a65c9c274d24e7c57d56a5333019d5aed8
Douglas Pimentel Teixeira <douglaspt@gmail.com>
2024-05-29 14:34:55 -0300

    Service layer, component registration

---
+++ src/main/java/com/dpt/cursospring/services/UserService.java
@@ -0,0 +1,27 @@
+package com.dpt.cursospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dpt.cursospring.entities.User;
+import com.dpt.cursospring.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
